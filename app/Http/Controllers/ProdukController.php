1c1c53a47c156f1b0ea8040adc1ea0b7cb7386ec
galennolan <galen.nolan1@gmail.com>
2023-05-23 16:24:09 +0700

    baru

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -13,5 +13,29 @@ class ProdukController extends Controller
         return response()->json(['data' => $produks]);
     }
 
-   
+    public function show($id)
+    {
+        $produk = Produk::findOrFail($id);
+        return response()->json(['data' => $produk]);
+    }
+
+    public function store(Request $request)
+    {
+        $produk = Produk::create($request->all());
+        return response()->json(['data' => $produk], 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $produk = Produk::findOrFail($id);
+        $produk->update($request->all());
+        return response()->json(['data' => $produk]);
+    }
+
+    public function destroy($id)
+    {
+        $produk = Produk::findOrFail($id);
+        $produk->delete();
+        return response()->json(['data' => $produk]);
+    }
 }
